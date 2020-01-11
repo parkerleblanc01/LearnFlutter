e6b8bfa1126c3efaf2d0a3634a0e9be2a3c6fa7d
Parker LeBlanc <parkerjleblanc@gmail.com>
2020-01-11 16:50:41 -0500

    Make the callback to add the transaction to the list

--- budget_app/lib/widgets/new_transaction.dart
+++ budget_app/lib/widgets/new_transaction.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+  final Function addTx;
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +32,10 @@ class NewTransaction extends StatelessWidget {
               child: Text('Add Transaction'),
               textColor: Colors.purple,
               onPressed: () {
-                print(titleController.text);
-                print(amountController.text);
+                addTx(
+                  titleController.text, 
+                  double.parse(amountController.text)
+                );
               },
             ),
           ],
--- budget_app/lib/widgets/user_transactions.dart
+++ budget_app/lib/widgets/user_transactions.dart
@@ -12,16 +12,36 @@ class UserTransactions extends StatefulWidget {
 class _UserTransactionsState extends State<UserTransactions> {
   final List<Transaction> _userTransactions = [
     Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
     Transaction(
-        id: 't2', title: 'Trader Joes', amount: 139.23, date: DateTime.now()),
+      id: 't2',
+      title: 'Trader Joes',
+      amount: 139.23,
+      date: DateTime.now(),
+    ),
   ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        NewTransaction(),
+        NewTransaction(_addNewTransaction),
         TransactionList(_userTransactions),
       ],
     );
