8555ade8097f37bbfa7bd2ca61314308512a57c3
Parker LeBlanc <parkerjleblanc@gmail.com>
2020-01-12 10:09:57 -0500

    Add a dart theme

--- budget_app/lib/widgets/transaction_list.dart
+++ budget_app/lib/widgets/transaction_list.dart
@@ -24,7 +24,7 @@ class TransactionList extends StatelessWidget {
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                       width: 2,
                     ),
                   ),
@@ -33,7 +33,7 @@ class TransactionList extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
