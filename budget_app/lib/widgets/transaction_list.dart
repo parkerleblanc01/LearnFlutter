28abfdc7eff73c6de9be7a2f80682fc1bbd2e866
Parker LeBlanc <parkerjleblanc@gmail.com>
2020-01-12 09:23:05 -0500

    Add validation to the form

--- budget_app/lib/widgets/transaction_list.dart
+++ budget_app/lib/widgets/transaction_list.dart
@@ -29,7 +29,7 @@ class TransactionList extends StatelessWidget {
                     ),
                   ),
                   child: Text(
-                    '\$${_userTransactions[index].amount}',
+                    '\$${_userTransactions[index].amount.toStringAsFixed(2)}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
