d42819564a349abec2e8fab7ed49f24473e20f6a
= <=>
2020-02-04 15:34:35 -0500

    Allow the user to switch between landscape mode and portrait mode. If in landscape, hide the chart unless the user wants to show it using a Switch element.

--- budget_app/lib/widgets/transaction_list.dart
+++ budget_app/lib/widgets/transaction_list.dart
@@ -12,21 +12,23 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return _userTransactions.isEmpty
-        ? Column(
-            children: <Widget>[
-              Text(
-                'No transactions added yet!',
-                style: Theme.of(context).textTheme.title,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 200,
-                child: Image.asset('assets/images/waiting.png'),
-              ),
-            ],
-          )
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset('assets/images/waiting.png'),
+                ),
+              ],
+            );
+          })
         : ListView.builder(
             itemBuilder: (context, index) {
               return Card(
