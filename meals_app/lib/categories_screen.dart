a647b1d7d5c0c25e9583afc139ee310b14ef063c
= <=>
2020-03-08 17:02:53 -0400

    Add new fonts, setup the theme for the application

--- meals_app/lib/categories_screen.dart
+++ meals_app/lib/categories_screen.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+
+import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[
-
-      ],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals App'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'MealsApp',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          body2: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          )
+        )
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
@@ -26,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DeliMeals'),
+        title: Text('MealsApp'),
       ),
       body: Center(
         child: Text('Navigation Time!'),
