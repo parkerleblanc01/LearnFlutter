ed6ca2413eec9768575e83a0ef82a177b2509010
= <=>
2020-04-25 17:22:56 -0400

    Add navigation to our application

---
+++ meals_app/lib/category_meals_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'The Receipes',
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'The Recipes For The Category!',
+        ),
+      ),
+    );
+  }
+}
