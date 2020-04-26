c171e2b39c91e42d08786c4a165868770ac58f0d
= <=>
2020-04-26 16:48:10 -0400

    Add data to the meals item list and download images from the web

---
+++ meals_app/lib/widgets/meal_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  static const double cardBorderRadius = 15;
+
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  MealItem({
+    @required this.title,
+    @required this.imageUrl,
+    @required this.duration,
+    @required this.complexity,
+    @required this.affordability,
+  });
+
+  void selectMeal() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(cardBorderRadius),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(cardBorderRadius),
+                    topRight: Radius.circular(cardBorderRadius),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
