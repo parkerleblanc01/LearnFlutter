32064b37670f46c0685108641fb04fca392cc3af
= <=>
2020-05-20 16:59:15 -0400

    Add validation and error catching to the login screen

--- shop_app/lib/providers/auth.dart
+++ shop_app/lib/providers/auth.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+// Models
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
@@ -10,15 +13,22 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(String urlSegment, String email, String password) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyD99WNGr5hJe9HuCjAcK5lkdE4PCRuiJyA';
-    final response = await http.post(
-      url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signUp(String email, String password) async {
