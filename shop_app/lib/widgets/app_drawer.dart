047865f4c2d1a37a26e77bcd0fa7ae9d230cf68d
= <=>
2020-05-23 18:29:11 -0400

    Add a logout function

--- shop_app/lib/widgets/app_drawer.dart
+++ shop_app/lib/widgets/app_drawer.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 // Screens
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
+
+// Providers
+import '../providers/auth.dart';
 
 
 class AppDrawer extends StatelessWidget {
@@ -37,6 +41,15 @@ class AppDrawer extends StatelessWidget {
             title: Text('Manage Products'),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pop();
+              Provider.of<Auth>(context, listen: false).logout();
             },
           ),
         ],
