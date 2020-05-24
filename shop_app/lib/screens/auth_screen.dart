6290f7bc0799f6a6c94ffb51df3b78d5076872ac
= <=>
2020-05-24 14:16:05 -0400

    Convert from animation builder to animatedContainer to manage the animation

--- shop_app/lib/screens/auth_screen.dart
+++ shop_app/lib/screens/auth_screen.dart
@@ -19,9 +19,7 @@ class AuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final deviceSize = MediaQuery
-        .of(context)
-        .size;
+    final deviceSize = MediaQuery.of(context).size;
     // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
     // transformConfig.translate(-10.0);
     return Scaffold(
@@ -53,7 +51,7 @@ class AuthScreen extends StatelessWidget {
                     child: Container(
                       margin: EdgeInsets.only(bottom: 20.0),
                       padding:
-                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
                       transform: Matrix4.rotationZ(-8 * pi / 180)
                         ..translate(-10.0),
                       decoration: BoxDecoration(
@@ -70,11 +68,7 @@ class AuthScreen extends StatelessWidget {
                       child: Text(
                         'MyShop',
                         style: TextStyle(
-                          color: Theme
-                              .of(context)
-                              .accentTextTheme
-                              .title
-                              .color,
+                          color: Theme.of(context).accentTextTheme.title.color,
                           fontSize: 50,
                           fontFamily: 'Anton',
                           fontWeight: FontWeight.normal,
@@ -129,7 +123,10 @@ class _AuthCardState extends State<AuthCard>
       begin: Size(double.infinity, 260),
       end: Size(double.infinity, 320),
     ).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn,),
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.fastOutSlowIn,
+      ),
     );
   }
 
@@ -142,19 +139,18 @@ class _AuthCardState extends State<AuthCard>
   void _showErrorDialog(String message) {
     showDialog(
       context: context,
-      builder: (ctx) =>
-          AlertDialog(
-            title: Text('An Error Occured!'),
-            content: Text(message),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('Okay'),
-                onPressed: () {
-                  Navigator.of(ctx).pop();
-                },
-              )
-            ],
-          ),
+      builder: (ctx) => AlertDialog(
+        title: Text('An Error Occured!'),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Okay'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
     );
   }
 
@@ -220,26 +216,20 @@ class _AuthCardState extends State<AuthCard>
 
   @override
   Widget build(BuildContext context) {
-    final deviceSize = MediaQuery
-        .of(context)
-        .size;
+    final deviceSize = MediaQuery.of(context).size;
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, child) =>
-            Container(
-//        height: _authMode == AuthMode.Signup ? 320 : 260,
-              height: _heightAnimation.value.height,
-              constraints:
-              BoxConstraints(minHeight: _heightAnimation.value.height),
-              width: deviceSize.width * 0.75,
-              padding: EdgeInsets.all(16.0),
-              child: child,
-            ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+//        height: _heightAnimation.value.height,
+        constraints: BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -274,19 +264,19 @@ class _AuthCardState extends State<AuthCard>
                 ),
                 _authMode == AuthMode.Signup
                     ? TextFormField(
-                  enabled: _authMode == AuthMode.Signup,
-                  decoration:
-                  InputDecoration(labelText: 'Confirm Password'),
-                  obscureText: true,
-                  validator: _authMode == AuthMode.Signup
-                      ? (value) {
-                    if (value != _passwordController.text) {
-                      return 'Passwords do not match!';
-                    }
-                    return null;
-                  }
-                      : null,
-                )
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration:
+                            InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                                return null;
+                              }
+                            : null,
+                      )
                     : Container(),
                 SizedBox(
                   height: 20,
@@ -294,35 +284,25 @@ class _AuthCardState extends State<AuthCard>
                 _isLoading
                     ? CircularProgressIndicator()
                     : RaisedButton(
-                  child: Text(
-                      _authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
-                  onPressed: _submit,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  padding: EdgeInsets.symmetric(
-                      horizontal: 30.0, vertical: 8.0),
-                  color: Theme
-                      .of(context)
-                      .primaryColor,
-                  textColor:
-                  Theme
-                      .of(context)
-                      .primaryTextTheme
-                      .button
-                      .color,
-                ),
+                        child: Text(
+                            _authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                        onPressed: _submit,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 30.0, vertical: 8.0),
+                        color: Theme.of(context).primaryColor,
+                        textColor:
+                            Theme.of(context).primaryTextTheme.button.color,
+                      ),
                 FlatButton(
                   child: Text(
-                      '${_authMode == AuthMode.Login
-                          ? 'SIGNUP'
-                          : 'LOGIN'} INSTEAD'),
+                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
                   onPressed: _switchAuthMode,
                   padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
                   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  textColor: Theme
-                      .of(context)
-                      .primaryColor,
+                  textColor: Theme.of(context).primaryColor,
                 ),
               ],
             ),
