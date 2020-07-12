ea8bbf98711d78534396d6e2fa1e6a1241469920
= <=>
2020-07-11 19:46:20 -0700

    create users using auth

--- chat_app/lib/screens/auth_screen.dart
+++ chat_app/lib/screens/auth_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 // Widgets
 import '../widgets/auth/auth_form.dart';
@@ -9,16 +11,44 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+
   void _submitAuthForm(
     String email,
     String password,
     String username,
     bool isLogin,
-  ) {
-    print(username);
-    print(email);
-    print(password);
-    // TODO: Send auth request to firebase
+    BuildContext ctx,
+  ) async {
+    AuthResult authResult;
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      }
+    } on PlatformException  catch (error) {
+      var message = 'An error occurred, please check your credentials!';
+      if (error.message != null) {
+        message = error.message;
+      }
+
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(ctx).errorColor,
+        ),
+      );
+    } catch (error) {
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(error.toString()),
+          backgroundColor: Theme.of(ctx).errorColor,
+        ),
+      );
+    }
   }
 
   @override
--- chat_app/lib/widgets/auth/auth_form.dart
+++ chat_app/lib/widgets/auth/auth_form.dart
@@ -6,6 +6,7 @@ class AuthForm extends StatefulWidget {
     String password,
     String username,
     bool isLogin,
+    BuildContext ctx,
   ) submitFn;
 
   AuthForm(this.submitFn);
@@ -27,7 +28,7 @@ class _AuthFormState extends State<AuthForm> {
 
     if (isValid) {
       _formKey.currentState.save();
-      widget.submitFn(_userEmail, _userPassword, _username, _isLogin);
+      widget.submitFn(_userEmail.trim(), _userPassword.trim(), _username.trim(), _isLogin, context);
     }
   }
 
