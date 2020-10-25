765cbf90fb0d5b12e6757d1d1650ea100603a9b0
= <=>
2020-10-25 14:26:18 -0700

    Add destinations carousel

--- flutter_travel/lib/screens/home_screen.dart
+++ flutter_travel/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel/widgets/destination_carousel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -28,13 +29,17 @@ class _HomeScreenState extends State<HomeScreen> {
         height: 60.0,
         width: 60.0,
         decoration: BoxDecoration(
-          color: _selectedIndex == index ? Theme.of(context).accentColor : Color(0xFFE7EBEE),
+          color: _selectedIndex == index
+              ? Theme.of(context).accentColor
+              : Color(0xFFE7EBEE),
           borderRadius: BorderRadius.circular(30.0),
         ),
         child: Icon(
           _icons[index],
           size: 25.0,
-          color: _selectedIndex == index ? Theme.of(context).primaryColor : Color(0xFFB4C1C4),
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Color(0xFFB4C1C4),
         ),
       ),
     );
@@ -65,9 +70,13 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: _icons.asMap().entries.map((e) => _buildIcon(e.key)).toList(),
-            )
-
+              children:
+                  _icons.asMap().entries.map((e) => _buildIcon(e.key)).toList(),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            DestinationCarousel(),
           ],
         ),
       ),
