f8809b3e1387ae7292ed7855d88afd2c9fff7875
= <=>
2020-11-27 10:39:05 -0700

    Add the list builder for the destination page

--- flutter_travel/lib/screens/destination_screen.dart
+++ flutter_travel/lib/screens/destination_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel/models/activity_model.dart';
 import 'package:flutter_travel/models/destination_model.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -33,7 +34,8 @@ class _DestinationScreenState extends State<DestinationScreen> {
                 child: Hero(
                   tag: widget.destination.imageUrl,
                   child: ClipRRect(
-                    borderRadius: BorderRadius.vertical(bottom: Radius.circular(30.0)),
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(30.0)),
                     child: Image(
                       image: AssetImage(
                         widget.destination.imageUrl,
@@ -74,7 +76,142 @@ class _DestinationScreenState extends State<DestinationScreen> {
                   ],
                 ),
               ),
+              Positioned(
+                left: 20.0,
+                bottom: 20.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.destination.city,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.locationArrow,
+                          size: 15.0,
+                          color: Colors.white70,
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          widget.destination.country,
+                          style:
+                              TextStyle(color: Colors.white70, fontSize: 20.0),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                bottom: 20.0,
+                child: Icon(
+                  Icons.location_on,
+                  color: Colors.white70,
+                  size: 25.0,
+                ),
+              ),
             ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.destination.activities.length,
+              itemBuilder: (context, index) {
+                Activity activity = widget.destination.activities[index];
+                return Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
+                      height: 170.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(100.0, 20.0, 20.0, 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 140.0,
+                                  child: Text(
+                                    activity.name,
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      '\$${activity.price}',
+                                      style: TextStyle(
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    Text(
+                                      'per pax',
+                                      style: TextStyle(color: Colors.grey),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Text(
+                              activity.type,
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            // _buildRatingStars(activity.rating),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: [
+                                Container(
+                                  width: 70.0,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).accentColor,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: Text(activity.startTimes[0]),
+                                ),
+                                SizedBox(width: 10.0),
+                                Container(
+                                  width: 70.0,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).accentColor,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: Text(activity.startTimes[1]),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
           ),
         ],
       ),
